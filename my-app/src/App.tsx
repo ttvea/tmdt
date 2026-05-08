4f32f54a933b7f93402caed0ad3a7ab693070188
PHUOCLOC107 <22130150@st.hcmuaf.edu.vn>
2026-05-08 10:32:50 +0700

    fe form tutor && profile

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,8 @@ import { Login } from "./pages/Login";
 import { OAuth2Redirect } from "./pages/OAuth2Redirect";
 import { Register } from "./pages/Register";
 import HomePage from "./pages/HomePage";
+import { TutorInfo } from "./pages/Tutor/TutorInfo";
+import { TutorProfile } from "./pages/Tutor/profile";
 
 function App() {
   const pathname = window.location.pathname;
@@ -11,6 +13,8 @@ function App() {
   if (pathname === "/login") return <Login />;
   if (pathname === "/register") return <Register />;
   if (pathname === "/oauth2/redirect") return <OAuth2Redirect />;
+  if (pathname === "/tutor/info") return <TutorInfo />;
+  if (pathname === "/tutor/profile") return <TutorProfile />;
 
   return <HomePage />;
 }
